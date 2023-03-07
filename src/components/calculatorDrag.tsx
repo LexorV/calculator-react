9d8013c125e913648877d279dc4e20e4be40abfc
Lex <Lexor.V.A.S@yandex.ru>
2023-03-07 21:01:44 +0300

    add field number

--- src/components/calculatorDrag.tsx
+++ src/components/calculatorDrag.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import TabletNumberInbox from './tabletNumberInbox';
 import TabletOperatorInbox from './tabletOperatorInbox';
 import { UniversalButton } from '../ui/buttons';
+import { TabletMain } from '../theme/globalComponentStyle';
 
 const CalculatorDragStyle = styled.div`
     display: flex;
@@ -11,17 +12,38 @@ const CalculatorDragStyle = styled.div`
     width: 240px;
     align-items: center;
 `;
+const ResultField = styled.div`
+margin: 4px;
+width: 232px;
+height: 52px;
+display: flex;
+background-color:#F3F4F6;
+justify-content: flex-end;
+font-weight: 800;
+font-size: 36px;
+box-sizing: border-box;
+align-items: center;
+border-radius: 4px;
+padding: 4px 8px;
+`;
 
 const CalculatorDrag: FC = () => (
   <CalculatorDragStyle>
+    <TabletMain>
+      <ResultField>
+        1
+      </ResultField>
+    </TabletMain>
     <TabletOperatorInbox />
     <TabletNumberInbox />
-    <UniversalButton
-      isEquals
-      height={64}
-      width={232}
-      value='='
-      onClick={(e) => console.log()} />
+    <TabletMain>
+      <UniversalButton
+        isEquals
+        height={64}
+        width={232}
+        value='='
+        onClick={(e) => console.log()} />
+    </TabletMain>
   </CalculatorDragStyle>
 );
 export default CalculatorDrag;
--- src/theme/globalComponentStyle.ts
+++ src/theme/globalComponentStyle.ts
@@ -7,5 +7,7 @@ export const TabletMain = styled.div`
     width: 240px;
     padding: 4px;
     box-sizing: border-box;
+    border-radius: 4px;
+    box-shadow: 0  4px 6px 0 rgba(0, 0, 0, 0.1), 0  2px 4px 0 rgba(0, 0, 0, 0.1);
 `;
 export default TabletMain;
